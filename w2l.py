add7e68988fde947949e18956e6f2124207b279f
Livio Bieri <livio@livio.li>
2020-08-05 23:32:15 +0200

    chore: use new model

--- w2l.py
+++ w2l.py
@@ -1,7 +1,9 @@
 from talon import speech_system
 from talon.engines.w2l import W2lEngine
 
-engine = W2lEngine(language='en_US', debug=False)
+# engine = W2lEngine(language='en_US-original-model', debug=False)
+engine = W2lEngine(language='en_US-sconv-large-b2', debug=True)
+
 # engine.register('', print)
 speech_system.add_engine(engine)
 speech_system.set_priority(['wav2letter'])
