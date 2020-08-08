8b8721bca49295b1f5dbf026d881d7e7c14f0783
Livio Bieri <livio@livio.li>
2020-08-08 12:39:45 +0200

    chore: fix models

--- w2l.py
+++ w2l.py
@@ -2,7 +2,8 @@ from talon import speech_system
 from talon.engines.w2l import W2lEngine
 
 # engine = W2lEngine(language='en_US-original-model', debug=False)
-engine = W2lEngine(language='en_US-sconv-large-b2', debug=True)
+# engine = W2lEngine(model='en_US-sconv-beta5', debug=False)
+engine = W2lEngine(model='en_US-sconv-large-b2', debug=False)
 
 # engine.register('', print)
 speech_system.add_engine(engine)
