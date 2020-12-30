d1a0cfaf8fdbb9d682b4f2baff24063ed474083d
Livio Bieri <livio@livio.li>
2020-12-30 16:54:54 +0100

    chore: bump to sconv-beta6

--- w2l.py
+++ w2l.py
@@ -3,7 +3,8 @@ from talon.engines.w2l import W2lEngine
 
 # engine = W2lEngine(language='en_US-original-model', debug=False)
 # engine = W2lEngine(model='en_US-sconv-beta5', debug=False)
-engine = W2lEngine(model='en_US-sconv-large-b2', debug=False)
+# engine = W2lEngine(model='en_US-sconv-large-b2', debug=False)
+engine = W2lEngine(model='en_US-sconv-beta6', debug=False)
 
 # engine.register('', print)
 speech_system.add_engine(engine)
